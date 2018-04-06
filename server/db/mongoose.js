cb53581b5c3c8069a4b0c87538cbc69bdbbf985e
DESKTOP-91F0BB8\stevn <stevnasi@gmail.com>
2018-04-06 14:25:15 +1000

    Modify mongo database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require ("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
+mongoose.connect("mongodb://stevenasi:stevmirna33@ds121225.mlab.com:21225/stevenasi" || "mongodb://localhost:27017/TodoApp");
 
 module.exports = {mongoose}
