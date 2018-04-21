3527f0acc335caacbd4db101a63fdba2b4846ef9
DESKTOP-91F0BB8\stevn <stevnasi@gmail.com>
2018-04-21 17:40:33 +1000

    Modify the expect test functions for latest version

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require ("mongoose");
 // var {MONGODB_URI} = require("../config/config").MONGODB_URI;
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGODB_URI || "mongodb://stevenasi:stevmirna33@ds121225.mlab.com:21225/stevenasi");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://stevenasi:stevmirna33@ds121225.mlab.com:21225/stevenasi");
 
 module.exports = {mongoose}
